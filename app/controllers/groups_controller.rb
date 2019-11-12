f781caeee55227cc9a47fecfab161b7790502d16
hagamisato <n64.haga.misato@gmail.com>
2019-11-12 21:39:24 +0900

    12日

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,20 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+end
